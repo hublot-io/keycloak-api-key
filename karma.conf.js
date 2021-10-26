2a53a6d5207e3338bcc2a9ca731472da64b19328
Nathan <nathan.grimaud@notilus.loc>
2021-10-26 13:30:48 +0000

    feat: browser support

--- karma.conf.js
+++ karma.conf.js
@@ -66,7 +66,7 @@ module.exports = function(config) {
         // start these browsers
         // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
         // browsers: ['ChromeHeadless', 'Firefox'],
-        browsers: ['ChromeHeadless'],
+        browsers: ['ChromeHeadless', 'Firefox'],
 
 
         // Continuous Integration mode
